59cc29f52e172f894fa187dd72c2d323cf4f0497
Muhammad Arif Hanif <arif.hanif3@gmail.com>
2015-09-12 14:50:19 -0400

    last version of routes and url_for

--- hello.py
+++ hello.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
+
+@app.route("/profile/<username>")
+def show_user_profile(username):
+    return "User: " + str(username)
+
+@app.route("/post/<int:post_id>")
+def show_user_post(post_id):
+    return "Post: " + str(post_id)
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def show_url_for():
+    return url_for('show_user_profile', username = 'arif')
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
