7bedcd1dbeabb9e0e1839fb37a4fffce2c73047b
Muhammad Arif Hanif <arif.hanif3@gmail.com>
2015-09-12 15:39:14 -0400

    HTTP Intro

--- hello.py
+++ hello.py
@@ -1,8 +1,14 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 app = Flask(__name__)
 
+@app.route('/login', methods=['GET'])
+def login():
+    if request.values:
+        return 'username is %s' % request.values['username']
+    else:
+        return '<form method="GET" action="/login"><input type = "text" name = "username"/><p><button type="submit">Submit</button></form>'
 
-@app.route("/profile/<username>")
+''' @app.route("/profile/<username>")
 def show_user_profile(username):
     return "User: " + str(username)
 
@@ -12,7 +18,7 @@ def show_user_post(post_id):
 
 @app.route("/")
 def show_url_for():
-    return url_for('show_user_profile', username = 'arif')
+    return url_for('show_user_profile', username = 'arif') '''
 
 if __name__ == "__main__":
     app.debug = True
