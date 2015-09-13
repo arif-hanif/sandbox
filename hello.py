8f50f1e61394d5855249a5e57810bd89d1e805ef
Muhammad Arif Hanif <arif.hanif3@gmail.com>
2015-09-13 08:47:18 -0400

    cookies
    implementation of cookies to track login username and deleting for
    logout

--- hello.py
+++ hello.py
@@ -1,17 +1,15 @@
-from flask import Flask, url_for, request, render_template, redirect, flash
+from flask import Flask, url_for, request, render_template, redirect, flash, make_response
 app = Flask(__name__)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST' :
-        if valid_login(
-            request.form.get('username'),
-            request.form.get('password')
-        ):
+        if valid_login(request.form.get('username'), request.form.get('password')):
             flash("Sucessfully Logged In")
-            flash("thanks for logging in")
-            return redirect(url_for('welcome', username=request.form.get('username')))
+            response = make_response(redirect(url_for('welcome')))
+            response.set_cookie('username', request.form.get('username'))
+            return response
         else:
             error = "Incorrect username and password"
     return render_template('login.html', error=error)
@@ -23,9 +21,19 @@ def valid_login(username, password):
     else:
         return False
 
-@app.route('/welcome/<username>')
-def welcome(username):
-    return render_template('welcome.html', username=username)
+@app.route('/logout')
+def logout():
+    response = make_response(redirect(url_for('login')))
+    response.set_cookie('username', '', expires=0)
+    return response
+
+@app.route('/')
+def welcome():
+    username = request.cookies.get('username')
+    if username:
+        return render_template('welcome.html', username=username)
+    else:
+        return redirect(url_for('login'))
 
 ''' @app.route("/profile/<username>")
 def show_user_profile(username):
