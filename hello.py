c98ac7452f378c57ddbe90194ac57e9ffe23a8b6
Muhammad Arif Hanif <arif.hanif3@gmail.com>
2015-09-12 23:52:53 -0400

    redirect implementation
    learned about redirect

--- hello.py
+++ hello.py
@@ -1,11 +1,29 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, url_for, request, render_template, redirect
 app = Flask(__name__)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
     if request.method == 'POST' :
-        return "User %s logged in" % request.form['username']
-    return render_template('login.html')
+        if valid_login(
+            request.form.get('username'),
+            request.form.get('password')
+        ):
+            return redirect(url_for('welcome', username=request.form.get('username')))
+        else:
+            error = "Incorrect username and password"
+    return render_template('login.html', error=error)
+
+def valid_login(username, password):
+    # checks on the db if the username and password are correct
+    if username == password:
+        return True
+    else:
+        return False
+
+@app.route('/welcome/<username>')
+def welcome(username):
+    return render_template('welcome.html', username=username)
 
 ''' @app.route("/profile/<username>")
 def show_user_profile(username):
